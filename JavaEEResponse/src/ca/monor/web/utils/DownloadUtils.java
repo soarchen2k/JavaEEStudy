3a85dc5fbb72f52a9d715a0b110477c0735fae26
soarchen <chenchong.blog@gmail.com>
2020-03-17 00:23:11 -0400

    iMac

--- JavaEEResponse/src/ca/monor/web/utils/DownloadUtils.java
+++ JavaEEResponse/src/ca/monor/web/utils/DownloadUtils.java
@@ -14,7 +14,7 @@ public class DownloadUtils {
             filename = filename.replace("+", " ");
         } else if (agent.contains("Firefox")) {
             BASE64Encoder base64Encoder = new BASE64Encoder();
-            filename = "=?utf-8?B?" + base64Encoder.encode(filename.getBytes("utf-8"));
+            filename = "=?utf-8?B?" + base64Encoder.encode(filename.getBytes("utf-8")) + "?=";
         } else {
             filename = URLEncoder.encode(filename, "utf-8");
         }
