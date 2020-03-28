2fa423342df06c9dd3c4b0456d50f62540bd2320
soarchen <chenchong.blog@gmail.com>
2020-03-27 23:22:13 -0400

    iMac

--- userAccountSystem/src/ca/monor/web/findUserByPageServlet.java
+++ userAccountSystem/src/ca/monor/web/findUserByPageServlet.java
@@ -1,11 +1,16 @@
 package ca.monor.web;
 
+import ca.monor.domain.PageBean;
+import ca.monor.domain.User;
+import ca.monor.service.impl.UserServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 @WebServlet("/findUserByPageServlet")
 public class findUserByPageServlet extends HttpServlet {
@@ -16,6 +21,21 @@ public class findUserByPageServlet extends HttpServlet {
         String currentPage = request.getParameter("currentPage");
         String rows = request.getParameter("rows");
 
+        if (currentPage == null || "".equals(currentPage)) { // 尽量写上判空条件
+            currentPage = "1";
+        }
+
+        if (rows == null || "".equals(rows)) {
+            rows = "5";
+        }
+
+        Map<String, String[]> conditions = request.getParameterMap();
+        // 2. 向后台请求数据
+        PageBean<User> pageBean = new UserServiceImpl().findUserByPage(currentPage, rows, conditions);
+
+        // 3. 将装载好的数据传入域中，供前端调用
+        request.setAttribute("pb", pageBean);
+        request.setAttribute("condition", conditions);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
